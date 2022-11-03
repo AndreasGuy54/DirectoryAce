36a77106a758a15890a942e5b3374ea810ba08c0
Andreas Abaidoo <abaidooandy@gmail.com>
2022-11-03 12:13:14 +0000

    Scaffolded Identity Pages and Extended Registration With First and Last Name

--- DirectoryAce/Program.cs
+++ DirectoryAce/Program.cs
@@ -1,4 +1,5 @@
 using DirectoryAce.Data;
+using DirectoryAce.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,14 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
