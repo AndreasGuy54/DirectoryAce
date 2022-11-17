d8267c3b3a2b964128ee20a262efb1dfb048dc56
Andreas Abaidoo <abaidooandy@gmail.com>
2022-11-17 11:04:23 +0000

    Built and Configured Mail Service

---
+++ DirectoryAce/Services/EmailService.cs
@@ -0,0 +1,57 @@
+﻿using DirectoryAce.Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace DirectoryAce.Services
+{
+    public class EmailService : IEmailSender
+    {
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var emailSender = _mailSettings.Email;
+
+            MimeMessage newEmail = new();
+            newEmail.Sender = MailboxAddress.Parse(emailSender);
+            newEmail.Subject = subject;
+
+            foreach (var emailAddress in email.Split(";"))
+            {
+                newEmail.To.Add(MailboxAddress.Parse(emailAddress));
+            }
+
+            BodyBuilder emailBody = new();
+            emailBody.HtmlBody = htmlMessage;
+            newEmail.Body = emailBody.ToMessageBody();
+
+            //login to smtp client at this point
+            using SmtpClient smtpClient = new();
+            try
+            {
+                var host = _mailSettings.Host;
+                var port = _mailSettings.Port;
+                var password = _mailSettings.Password;
+
+                await smtpClient.ConnectAsync(host, port, SecureSocketOptions.StartTls);
+                await smtpClient.AuthenticateAsync(emailSender, password);
+
+                //send the message
+                await smtpClient.SendAsync(newEmail);
+                await smtpClient.DisconnectAsync(true);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
