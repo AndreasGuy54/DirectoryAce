2788ae611acced4a8f4b818943df417aecb0c16f
Andreas Abaidoo <abaidooandy@gmail.com>
2022-11-19 07:19:37 +0000

    Edited categories index page

--- DirectoryAce/Controllers/CategoriesController.cs
+++ DirectoryAce/Controllers/CategoriesController.cs
@@ -8,24 +8,31 @@ using Microsoft.EntityFrameworkCore;
 using DirectoryAce.Data;
 using DirectoryAce.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace DirectoryAce.Controllers
 {
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
 
-        public CategoriesController(ApplicationDbContext context)
+        public CategoriesController(ApplicationDbContext context, UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Categories
         [Authorize]
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Categories.Include(c => c.AppUser);
-            return View(await applicationDbContext.ToListAsync());
+            string appUserId = _userManager.GetUserId(User);
+
+            var categories = await _context.Categories.Where(c => c.AppUserId == appUserId)
+                                                .Include(c => c.AppUser).ToListAsync();
+                                                
+            return View(categories);
         }
 
         // GET: Categories/Details/5
